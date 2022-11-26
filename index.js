0945944a3846f0090781e433a69aedae169bf31d
sk1f112 <vlad.ostapen15@gmail.com>
2022-11-26 19:02:50 +0200

    modal in modal

--- index.js
+++ index.js
@@ -109,4 +109,137 @@ if(animItem.length > 0){
   }, 300);
 }
 
+/***************************************************Sticky header *****************************************************************/
+const heroSection = document.querySelector('.section__hero');
+const header = document.querySelector('.header');
+const sections = document.querySelectorAll('.section');
+const navLinks = document.querySelectorAll('.nav__link');
+const navLi = document.querySelectorAll('.nav__list-item');
 
+document.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('scroll', function(){
+    let scrollTop = window.scrollY;
+    if(scrollTop >= 500){
+      header.classList.add('fixed');
+      heroSection.style.paddingTop = `${header.offsetHeight}px`;
+    } else {
+      header.classList.remove('fixed');
+      heroSection.style.paddingTop = `0px`;
+    }
+    //Active menu item when scroll
+    sections.forEach((el, i) => {
+      if(el.offsetTop - header.clientHeight <= scrollTop){
+        navLinks.forEach(el => {
+          if(el.classList.contains('active')){
+            el.classList.remove('active');
+          } 
+        })
+        navLi[i].querySelector('a').classList.add('active');
+      }
+    })
+  })
+})
+
+//Modal
+const btnModal = document.querySelector('.btn__header');
+const modal = document.querySelector('.popup');
+const popupContainer = document.querySelector('.popup__container');
+const modalShadow = document.querySelector('.shadow');
+
+let paddingOffSet = `${window.innerWidth - document.body.offsetWidth}px`;
+
+let html;
+function modalActive(modalType){
+  if(modalType == "login"){
+      popupContainer.removeChild(document.querySelector('.popup__content'))
+
+      html = `
+      <div class="popup__content">
+        <h2 class="popup__title">Log in to your account</h2>
+        <div class="popup__loggin-first">
+            <a href="" class="popup__facebook">
+                <img src="./assets/facebook.png" alt="facebook">
+                Sign In with Facebook
+            </a>
+            <a href="" class="popup__google">
+                <img src="./assets/google.png" alt="google">
+                Sign In with Google
+            </a>
+        </div>
+        <div class="popup__or">
+            <h3 class="popup__or-text">or</h3>
+            <span class="popup__line"></span>
+        </div>
+        <form action="#" class="popup__inputs inputs">
+            <div class="inputs__container">
+                <label for="email">Email</label>
+                <input type="text" class="input" id="email">
+            </div>
+            <div class="inputs__container">
+                <label for="password">Passoword</label>
+                <input type="password" class="input" id="password">
+            </div>
+
+            <button class="modal__btn">Sign in</button>
+        </form>
+        <div class="modal__footer">
+            <a href="#" class="modal__forgot">Forgot Your Password?</a>
+            <span class="popup__line forgot"></span>
+        </div>
+        <div class="modal__text">Don’t have an account? <a href="#" class="modal__register"
+       onclick="modalActive('reg')">Register</a></div>
+      </div>
+      `
+  } 
+  if(modalType == 'reg'){
+    if(document.querySelector('.popup__content')){
+    popupContainer.removeChild(document.querySelector('.popup__content'))
+
+    }
+
+    html = `
+    <div class="popup__content">
+      <h2 class="popup__title">Create account</h2>
+      <form action="#" class="popup__inputs inputs">
+            <div class="inputs__container">
+                <label for="email">Email</label>
+                <input type="text" class="input" id="email">
+            </div>
+            <div class="inputs__container">
+                <label for="password">Passoword</label>
+                <input type="password" class="input" id="password">
+            </div>
+            <button class="modal__btn">Sign up</button>
+        </form>
+        <div class="modal__footer">
+            <span class="popup__line reg"></span>    
+            <div class="modal__text">Already have an account? <a href="#" onClick="modalActive('login')" class="modal__register">Log in</a></div> 
+        </div>
+    </div>
+    `
+  }
+  popupContainer.insertAdjacentHTML('afterbegin',html)
+
+
+  if(!modal.classList.contains('active')){
+    modal.classList.add('active');
+    modalShadow.classList.add('active'); 
+    document.body.classList.add('lock')
+    if(header.classList.contains('fixed')){
+      header.style.paddingRight = paddingOffSet;
+    }
+    document.body.style.paddingRight = paddingOffSet;
+  } 
+}
+  
+function modalDisable(){
+  modal.classList.remove('active');
+  modalShadow.classList.remove('active');  
+  document.body.classList.remove('lock');
+  if(header.classList.contains('fixed')){
+    header.style.paddingRight = `0px`;
+  }
+  document.body.style.paddingRight = `0px`;
+};
+
+modalShadow.addEventListener('click', modalDisable)
